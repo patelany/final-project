3b9b5b59f1430b2f20a637b64d853fdcf021db02
chicletn <noelledevine@gmail.com>
2023-06-14 15:49:47 -0400

    Wednesday 614

---
+++ src/components/Login.tsx
@@ -0,0 +1,48 @@
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
+import Icon from "@mdi/react";
+import { mdiNotebookEditOutline } from "@mdi/js";
+import { signInWithGoogle, signOut } from "../firebaseConfig";
+import noteBook from "../images/noteBook.png";
+
+const Login = () => {
+  const { user } = useContext(AuthContext);
+  console.log(user);
+
+  return (
+    <header className="Login">
+      <h1>
+        <p>FACTT</p>
+      </h1>
+      <h2>Food Allergy Child Trial Tracker</h2>
+      <h2>
+        <img src={noteBook}></img>
+      </h2>
+      {user ? (
+        <div className="loggedIn">
+          <p>Welcome: {user?.displayName}</p>
+          <p>
+            <img
+              className="profileImage"
+              src={user.photoURL || ""}
+              alt="profile"
+            />
+          </p>
+          <button className="addChild">Add A Child</button>
+          <button className="signOut" onClick={() => signOut()}>
+            Sign Out
+          </button>
+        </div>
+      ) : (
+        <div className="loggedOut">
+          <p>Please Sign In</p>
+          <button className="signIn" onClick={() => signInWithGoogle()}>
+            Sign In
+          </button>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Login;
