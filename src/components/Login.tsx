f31ee62161952aaaa8cfd5c2efaf0246e37b9808
unknown <erikakyles@hotmail.com>
2023-06-15 16:54:04 -0400

    thursday

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,21 @@
-import { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect } from "react";
 import AuthContext from "../context/AuthContext";
 import Icon from "@mdi/react";
 import { mdiNotebookEditOutline } from "@mdi/js";
 import { signInWithGoogle, signOut } from "../firebaseConfig";
 import noteBook from "../images/noteBook.png";
+import "./Login.css";
 
 const Login = () => {
   const { user } = useContext(AuthContext);
   console.log(user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user) {
+      navigate("/Home");
+    }
+  }, [user]);
 
   return (
     <header className="Login">
@@ -18,29 +26,13 @@ const Login = () => {
       <h2>
         <img src={noteBook}></img>
       </h2>
-      {user ? (
-        <div className="loggedIn">
-          <p>Welcome: {user?.displayName}</p>
-          <p>
-            <img
-              className="profileImage"
-              src={user.photoURL || ""}
-              alt="profile"
-            />
-          </p>
 
-          <button className="signOut" onClick={() => signOut()}>
-            Sign Out
-          </button>
-        </div>
-      ) : (
-        <div className="loggedOut">
-          <p>Please Sign In</p>
-          <button className="signIn" onClick={() => signInWithGoogle()}>
-            Sign In
-          </button>
-        </div>
-      )}
+      <div className="loggedOut">
+        <p>Please Sign In</p>
+        <button className="signIn" onClick={() => signInWithGoogle()}>
+          Sign In
+        </button>
+      </div>
     </header>
   );
 };
--- src/services/patientApiService.ts
+++ src/services/patientApiService.ts
@@ -6,3 +6,13 @@ const baseUrl: string = process.env.REACT_APP_API_URL || "";
 export const addPatient = async (newPatient: Patient): Promise<Patient> => {
   return (await axios.post(`${baseUrl}/patient`, newPatient)).data;
 };
+
+export const getPatient = async (id: string): Promise<Patient[]> => {
+  return (await axios.get(`${baseUrl}/allPatients/${encodeURIComponent(id)}`))
+    .data;
+};
+export const getPatientByGuardian = async (id: string): Promise<Patient[]> => {
+  return (
+    await axios.get(`${baseUrl}/allPatients/guardian/${encodeURIComponent(id)}`)
+  ).data;
+};
